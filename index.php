becd8d12105e2fb3d112e5dee23368062726bb60
alirezakazemi47 <alirezakazemi47@gmail.com>
2022-03-29 13:12:50 +0430

    End Section Under Baner Image

--- index.php
+++ index.php
@@ -5,17 +5,35 @@
     <div class="banerImage__text">
         <div class="text__textNumberOne">
             <p>
-                آندرومدا واچ 
+                آندرومدا واچ
             </p>
         </div>
         <div class="text__textNumberTwo">
             <p>
-                نه تنها زمان را نشان می دهد 
+                نه تنها زمان را نشان می دهد
             </p>
         </div>
         <div class="text__text__numberThere">
             <p>
-                بلکه تاریخ را نیز بازگو میکند 
+                بلکه تاریخ را نیز بازگو میکند
+            </p>
+        </div>
+    </div>
+</div>
+<div class="underBaner">
+    <div class="UnderBaner__text">
+        <div class="text__special">
+            <p>ویژه</p>
+        </div>
+        <div class="text__luxury">
+            <p>لوکس</p>
+        </div>
+        <div class="text__bestSeller">
+            <p>پر فروش</p>
+        </div>
+        <div class="text__news">
+            <p>
+                جدیدترین
             </p>
         </div>
     </div>
